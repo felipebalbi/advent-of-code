9cbe6eab0d09fbae51ac7fb621cd03dcd7c85ac8
Felipe Balbi <felipe@balbi.sh>
2021-12-02 20:37:58 +0200

    2021: 1: Add part 2
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- 2021/1/solve.hs
+++ 2021/1/solve.hs
@@ -1,3 +1,6 @@
+addEachTriple :: [Int] -> [Int]
+addEachTriple ns = zipWith3 (\a b c -> a + b + c) ns (tail ns) (tail . tail $ ns)
+
 solve :: [Int] -> Int
 solve ns = length $ filter (<0) $ zipWith (-) ns (tail ns)
 
@@ -5,4 +8,4 @@ convert :: [String] -> [Int]
 convert = map read
 
 main :: IO ()
-main = interact $ show . solve . convert . lines
+main = interact $ show . solve . addEachTriple . convert . lines
