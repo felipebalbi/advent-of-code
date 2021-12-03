b51a93c31deb8a9f778e60d73b96faee5273316e
Felipe Balbi <felipe@balbi.sh>
2021-12-03 11:23:21 +0200

    2021: 3: part two
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- 2021/3/solve.hs
+++ 2021/3/solve.hs
@@ -11,10 +11,24 @@ mostCommon = head . maximumBy (compare `on` length) . group . sort
 leastCommon :: Ord a => [a] -> a
 leastCommon = head . minimumBy (compare `on` length) . group . sort
 
+oxygen :: [String] -> Int
+oxygen strs = binToInt $ go 0 ((length (transpose strs)) - 1) strs
+  where
+    go pos maxPos xs = top : if   pos < maxPos
+                             then go (pos + 1) maxPos (filter (\str -> (str !! pos) == top) xs)
+                             else []
+      where top = mostCommon $ (!!pos) (transpose xs)
+
+co2 :: [String] -> Int
+co2 strs = binToInt $ go 0 ((length (transpose strs)) - 1) strs
+  where
+    go pos maxPos xs = bottom : if   pos < maxPos
+                             then go (pos + 1) maxPos (filter (\str -> (str !! pos) == bottom) xs)
+                             else []
+      where bottom = leastCommon $ (!!pos) (transpose xs)
+
 solve :: [String] -> Int
-solve nums = epsilon * gamma
-  where epsilon = binToInt . map mostCommon . transpose $ nums
-        gamma = binToInt . map leastCommon . transpose $ nums
+solve nums = (oxygen nums) * (co2 nums)
 
 main :: IO ()
 main = interact $ show . solve . lines
