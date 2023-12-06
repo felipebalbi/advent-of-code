809c5e1cd6a99914e26d2b12da7768af0f21a636
Felipe Balbi <felipe@balbi.sh>
2023-12-05 20:38:02 -0800

    template: only one input required
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

--- template/src/main.rs
+++ template/src/main.rs
@@ -5,12 +5,11 @@ use {{project-name}}::{part1, part2};
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    let input1 = include_str!("../input1.txt");
-    let result = part1(input1)?;
+    let input = include_str!("../input.txt");
+    let result = part1(input)?;
     println!("Part 1: {result}");
 
-    let input2 = include_str!("../input2.txt");
-    let result = part2(input2)?;
+    let result = part2(input)?;
     println!("Part 2: {result}");
 
     Ok(())
